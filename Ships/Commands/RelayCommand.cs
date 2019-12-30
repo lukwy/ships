32519c625ed504d18a9223f9d421883a9bd4bb06
lukwy <wypchlo.lukasz@gmail.com>
2019-12-30 17:33:57 +0100

    RelayCommand cleaning

--- Ships/Commands/RelayCommand.cs
+++ Ships/Commands/RelayCommand.cs
@@ -9,22 +9,14 @@ namespace Ships.Commands
 {
     public class RelayCommand : ICommand
     {
-        //delegate command to register method to be executed
         private readonly Action handler;
         private bool isEnabled;
 
-        /// <summary>
-        /// Bind method to be executed to the handler
-        /// So that it can direct on event execution
-        /// </summary>
-        /// <param name="handler"></param>
         public RelayCommand(Action handler)
         {
-            // Assign the method name to the handler
             this.handler = handler;
         }
 
-        //Property that helps to
         public bool IsEnabled
         {
             get { return isEnabled; }
@@ -41,11 +33,6 @@ namespace Ships.Commands
             }
         }
 
-        /// <summary>
-        /// method to specify if the event will execute
-        /// </summary>
-        /// <param name="parameter"></param>
-        /// <returns></returns>
         public bool CanExecute(object parameter)
         {
             return IsEnabled;
@@ -53,10 +40,8 @@ namespace Ships.Commands
 
         public event EventHandler CanExecuteChanged;
 
-        // Helps to execute the respective method using the handler
         public void Execute(object parameter)
         {
-            //calls the respective method that has been registered with the handler
             handler();
         }
     }
