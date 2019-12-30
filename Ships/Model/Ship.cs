19a5c90181e3fa3fb7ac77b2c5be776cb073f1c5
lukwy <wypchlo.lukasz@gmail.com>
2019-12-30 17:04:14 +0100

    Play button added

--- Ships/Model/Ship.cs
+++ Ships/Model/Ship.cs
@@ -1,35 +1,62 @@
-﻿using System.ComponentModel;
+﻿using Ships.Commands;
+using System.ComponentModel;
+using System.Windows.Media;
 
 namespace Ships.Model
 {
-    class Ship : INotifyPropertyChanged
+    public class Ship : INotifyPropertyChanged
     {
-        private string _Name;
-        public string Name
+        public RelayCommand ButtonClick { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Ship()
+        {
+            ButtonClick = new RelayCommand(ChangeColor);
+            ButtonClick.IsEnabled = true;
+        }
+
+        protected void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private void ChangeColor()
+        {
+            if(IsGameStarted)
+            {
+                Color = IsShip ? Brushes.Chartreuse : Brushes.Red;
+            }
+            else
+            {
+                Color = Color == Brushes.White ? Brushes.Chartreuse : Brushes.White;
+                IsShip = !IsShip;
+            }
+        }
+
+        public void SetWhite()
+        {
+            Color = Brushes.White;
+        }
+
+        private Brush _Color;
+        public Brush Color
         {
             get
             {
-                return _Name;
+                return _Color;
             }
             set
             {
-                _Name = value;
-                OnPropertyChanged("Name");
+                _Color = value;
+                NotifyPropertyChanged("Color");
             }
         }
 
-        #region INotifyPropertyChanged Members
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        private void OnPropertyChanged(string propertyName)
-        {
-            PropertyChangedEventHandler handler = PropertyChanged;
-
-            if (handler != null)
-            {
-                handler(this, new PropertyChangedEventArgs(propertyName));
-            }
-        }
-        #endregion
+        public bool IsShip { get; private set; }
+        public bool IsGameStarted { get; set; }
     }
 }
--- Ships/ViewModel/BattleShipViewModel.cs
+++ Ships/ViewModel/BattleShipViewModel.cs
@@ -1,11 +1,49 @@
-﻿namespace Ships.ViewModel
+﻿using Ships.Model;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows.Input;
+using System.Windows.Media;
+using Ships.Commands;
+
+namespace Ships.ViewModel
 {
     internal class BattleShipViewModel
     {
-        public bool CanUpdate
+        public BattleShipViewModel()
         {
-            get;
-            set;
+            _ShipsData = new ObservableCollection<Ship>();
+            Generate();
+            StartCommand = new RelayCommand(StartGame);
+            StartCommand.IsEnabled = true;
+        }
+
+        private ObservableCollection<Ship> _ShipsData;
+        public ObservableCollection<Ship> ShipsData { get { return _ShipsData; } }
+
+        public void Generate()
+        {
+            _ShipsData.Clear();
+
+            for (int i = 0; i < 400; i++)
+            {
+                var temp = new Ship();
+                temp.SetWhite();
+                _ShipsData.Add(temp);
+            }
+        }
+
+        public RelayCommand StartCommand { get; set; }
+
+        private void StartGame()
+        {
+            if (ShipsData.Any(s=>s.IsShip))
+            {
+                foreach (var ship in ShipsData)
+                {
+                    ship.IsGameStarted = true;
+                    ship.SetWhite();
+                }
+            }
         }
     }
 }
