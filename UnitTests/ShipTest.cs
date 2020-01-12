d4f3c2edc2a21f2d362727918ae71378da5bc484
lukwy <wypchlo.lukasz@gmail.com>
2020-01-12 18:19:02 +0100

    Test added

---
+++ UnitTests/ShipTest.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Ships.Model;
+using System.Windows.Media;
+
+namespace UnitTests
+{
+    [TestClass]
+    class ShipTest
+    {
+        [TestMethod]
+        public void SetWhiteShould()
+        {
+            Mock<Ship> shipMock = new Mock<Ship>();
+
+            shipMock.Setup(s => s.SetWhite());
+
+            Brush whiteBrush = Brushes.White;
+
+            Assert.AreEqual(shipMock.Object.Color, whiteBrush);
+        }
+
+        [TestMethod]
+        public void ChangeColorOnGreenBeforeGameStart()
+        {
+            Mock<Ship> shipMock = new Mock<Ship>();
+
+            shipMock.Setup(s => s.SetWhite());
+            shipMock.Setup(s => s.ButtonClick);
+
+            Brush greenColor = Brushes.Chartreuse;
+
+            Assert.AreEqual(shipMock.Object.Color, greenColor);
+        }
+
+        [TestMethod]
+        public void ChangeColorOnRedAfterGameStart()
+        {
+            Mock<Ship> shipMock = new Mock<Ship>();
+
+            shipMock.Setup(s => s.SetWhite());
+            shipMock.Object.IsGameStarted = true;
+            shipMock.Setup(s => s.ButtonClick);
+
+            Brush redColor = Brushes.Red;
+
+            Assert.AreEqual(shipMock.Object.Color, redColor);
+        }
+    }
+}
