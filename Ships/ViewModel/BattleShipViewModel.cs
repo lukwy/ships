18bda71008fc5b2f2be8409d7834175a291958dd
lukwy <wypchlo.lukasz@gmail.com>
2020-01-26 17:56:02 +0100

    Init call added

--- Ships/ViewModel/BattleShipViewModel.cs
+++ Ships/ViewModel/BattleShipViewModel.cs
@@ -10,6 +10,8 @@ namespace Ships.ViewModel
         {
             ITextCounter message = new TextCounter();
             GameSetter = new GameSetter(message);
+
+            Init();
         }
 
         public void Init()
